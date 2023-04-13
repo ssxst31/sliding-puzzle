2d8d7579a5aab0aa97f30a16a1c00c13acc68699
star <chlvkfksquf@gmail.com>
2023-04-13 15:34:18 +0900

    Add profile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,35 @@ import { useState, useEffect } from "react";
 
 import "./index.css";
 
+const PROFILELIST = [
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/386388/1r3ni1l9h2fos6.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/248490/4uu5je1lda7mzsk.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/profile/e8co1kxoiemqx.png?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/698010/3uffg1lfw305ca.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/82975/3y3661lfzjzgd8.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/217830/3q5vs1l6dcpkwp.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/409224/g8ole1lg1bsejj.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/352809/3spu01ld49mxmg.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/profile/44chb1kqavbxbc.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/280457/1fxo1ld416ux0.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/2136/dsvda1lcqov8l9.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/681226/9ysyb71lfwahip9.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/393799/dznv1lcrl0pqn.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/profile/d3y9j1l323ejov.png?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/385586/40c7g1lfy86nco.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/93806/isq1lfgocy78.jpg?type=w&w=180&quality=90",
+  "https://jcmkeuseuzeq6748407.cdn.ntruss.com/members/691316/ibk1lf813wi4.jpg?type=w&w=180&quality=90",
+];
+
 function App() {
+  const result: any = [];
+  while (result.length < 4) {
+    const randomIndex = Math.floor(Math.random() * PROFILELIST.length);
+    const randomElement = PROFILELIST[randomIndex];
+    if (!result.includes(randomElement)) {
+      result.push(randomElement);
+    }
+  }
   const [tileList, setTileList] = useState<any>([]);
 
   function isSolvable(tiles: any) {
@@ -34,22 +62,22 @@ function App() {
   useEffect(() => {
     setTileList(
       shuffle([
-        { title: "2", value: [2, 1] },
-        { title: "1", value: [1, 1] },
-        { title: "4", value: [4, 1] },
-        { title: "5", value: [1, 2] },
-        { title: "0", value: [3, 1] },
-        { title: "6", value: [2, 2] },
-        { title: "3", value: [3, 2] },
-        { title: "8", value: [4, 2] },
-        { title: "9", value: [1, 3] },
-        { title: "10", value: [2, 3] },
-        { title: "7", value: [3, 3] },
-        { title: "12", value: [4, 3] },
-        { title: "13", value: [1, 4] },
-        { title: "14", value: [2, 4] },
-        { title: "11", value: [3, 4] },
-        { title: "15", value: [4, 4] },
+        { title: "1", value: [1, 1], img: result[0] },
+        { title: "2", value: [1, 2], img: result[0] },
+        { title: "3", value: [1, 3], img: result[0] },
+        { title: "0", value: [1, 4], img: result[0] },
+        { title: "5", value: [2, 1], img: result[1] },
+        { title: "6", value: [2, 2], img: result[1] },
+        { title: "4", value: [2, 3], img: result[1] },
+        { title: "8", value: [2, 4], img: result[1] },
+        { title: "9", value: [3, 1], img: result[2] },
+        { title: "10", value: [3, 2], img: result[2] },
+        { title: "12", value: [3, 3], img: result[2] },
+        { title: "7", value: [3, 4], img: result[2] },
+        { title: "13", value: [4, 1], img: result[3] },
+        { title: "15", value: [4, 2], img: result[3] },
+        { title: "11", value: [4, 3], img: result[3] },
+        { title: "14", value: [4, 4], img: result[3] },
       ])
     );
   }, []);
@@ -102,30 +130,36 @@ function App() {
 
   return (
     <div>
-      <div className="w-full bg-red-400 h-10 flex justify-center items-center">
-        <div className="text-white text-xl">
+      <div className="flex items-center justify-center w-full h-10 bg-red-400">
+        <div className="text-xl text-white">
           flex 슬라이딩 퍼즐입니다람쥐 많관부~
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-y-8 gap-x-8 ">
+      <div className="grid grid-cols-4 grid-rows-4 gap-y-8 gap-x-8 h-[calc(100vh-40px)]">
         {tileList.map((el: any) => {
           if (el.title === "0") {
             return (
               <div
-                className=" bg-white w-full h-full cursor-pointer"
+                className="w-full h-full bg-white cursor-pointer "
                 key={el.title}
               />
             );
           } else {
             return (
               <div
-                className="bg-red-300 w-full h-44 cursor-pointer border-black  rounded-md inline-flex items-center justify-center text-8xl"
+                className="relative inline-flex items-center justify-center w-full bg-red-300 border-black rounded-md shadow-lg cursor-pointer"
                 key={el.title}
                 onClick={() => {
                   moveTile(el);
                 }}
               >
-                {el.title}
+                <img
+                  className="absolute w-3/6 rounded-lg shadow-lg h-3/6"
+                  src={el.img}
+                />
+                <div className="absolute top-0 text-6xl text-white">
+                  {el.title}
+                </div>
               </div>
             );
           }
